fcd8152195a3bda9a1bd9772a7f81bd1d480ae6e
zuko0006 <zuko0006@algonquinlive.com>
2017-03-10 19:02:48 -0500

    Add files via upload

---
+++ ZukowskiMidtermWebApp/app.js
@@ -0,0 +1,55 @@
+
+//Define angular app
+var app = angular.module('TaskManager', []); 
+
+//controllers
+app.controller('taskController', function($scope) {
+    $scope.today = new Date();
+    $scope.saved = localStorage.getItem('taskItems');
+    $scope.taskItem = (localStorage.getItem('taskItems')!==null) ? 
+    JSON.parse($scope.saved) : [ {description: "Why not add a task?", date: $scope.today, complete: false}];
+    localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
+    
+    $scope.newTask = null;
+    $scope.newTaskDate = null;
+    $scope.addNew = function () {
+        if ($scope.newTaskDate == null || $scope.newTaskDate == '') {
+            $scope.taskItem.push({
+                description: $scope.newTask,
+                date: "No deadline",
+                complete: false,
+                category: $scope.newTaskCategory.name
+            }) 
+        } else {
+            $scope.taskItem.push({
+                description: $scope.newTask,
+                date: $scope.newTaskDate,
+                complete: false,
+                
+            })
+        };
+        $scope.newTask = '';
+        $scope.newTaskDate = '';
+        
+        localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
+    };
+    $scope.deleteTask = function () {
+        var completedTask = $scope.taskItem;
+        $scope.taskItem = [];
+        angular.forEach(completedTask, function (taskItem) {
+            if (!taskItem.complete) {
+                $scope.taskItem.push(taskItem);
+            }
+        });
+        localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
+    };
+    
+    $scope.editTask = function () {
+        var editTask = $scope.taskItem;
+        
+    }
+    
+    $scope.save = function () {
+        localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
+    }
+});
